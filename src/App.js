818b92bfef87407e03a7130a182dbc71263a822f
Randeep Singh <rand-singh@users.noreply.github.com>
2020-12-29 19:31:15 +0000

    Setup components and pages folder, created About Us portion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+// Import Pages
+import AboutUs from './pages/AboutUs'
+
 function App() {
-  return (
-    <div className="app">
-      <h1>App</h1>
-    </div>
-  );
+  	return (
+    	<div className="app">
+      		<AboutUs />
+    	</div>
+  	);
 }
 
 export default App;
---
+++ src/components/AboutSection.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import home1 from '../img/home1.png'
+
+function AboutSection() {
+    return (
+        <div>
+            <div className="description">
+                <div className="title">
+                    <div className="hide">
+                        <h2>We work to make </h2>
+                    </div>
+                    
+                    <div className="hide">
+                        <h2>your <span>dreams</span></h2>
+                    </div>
+                    
+                    <div className="hide">
+                        <h2>come true.</h2>
+                    </div>
+                </div>
+                <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills</p>
+                <button>Contact Us</button>
+                <div className="image">
+                    <img src={home1} alt="Main Image"/>
+                </div>
+            </div>            
+        </div>
+    )
+}
+
+export default AboutSection
