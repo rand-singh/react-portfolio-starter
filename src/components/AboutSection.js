d3cbf6cb0779a22b13a7a873e6bfe9a88f4a0bca
Randeep Singh <rand-singh@users.noreply.github.com>
2020-12-29 20:12:36 +0000

    Added FAQ section component

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,27 +3,25 @@ import home1 from '../img/home1.png'
 
 function AboutSection() {
     return (
-        <div>
-            <div className="description">
-                <div className="title">
-                    <div className="hide">
-                        <h2>We work to make </h2>
-                    </div>
-                    
-                    <div className="hide">
-                        <h2>your <span>dreams</span></h2>
-                    </div>
-                    
-                    <div className="hide">
-                        <h2>come true.</h2>
-                    </div>
+        <div className="description">
+            <div className="title">
+                <div className="hide">
+                    <h2>We work to make </h2>
                 </div>
-                <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills</p>
-                <button>Contact Us</button>
-                <div className="image">
-                    <img src={home1} alt="Photographer"/>
+                
+                <div className="hide">
+                    <h2>your <span>dreams</span></h2>
                 </div>
-            </div>            
+                
+                <div className="hide">
+                    <h2>come true.</h2>
+                </div>
+            </div>
+            <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills</p>
+            <button>Contact Us</button>
+            <div className="image">
+                <img src={home1} alt="Photographer"/>
+            </div>
         </div>
     )
 }
---
+++ src/components/FaqSection.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function FaqSection() {
+    return (
+        <div className="faq">
+            <h1>Any Questions <span>FAQ</span></h1>
+            <div className="qustion">
+                <h4>How do it start?</h4>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, explicabo?</p>
+            </div>
+
+            <div className="qustion">
+                <h4>Daily Schedule</h4>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, explicabo?</p>
+            </div>
+
+            <div className="qustion">
+                <h4>Different Payment Methods</h4>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, explicabo?</p>
+            </div>
+
+            <div className="qustion">
+                <h4>What Products do you offer?</h4>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, explicabo?</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default FaqSection
