174ff7f482240508ec8b236b6aa39d6cdb327cd4
Randeep Singh <rand-singh@users.noreply.github.com>
2021-02-14 18:26:22 +0000

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,8 @@ import { Link } from 'react-router-dom'
 function Nav() {
     return (
         <StyledNav>
-            <h1><a href="#" id="logo">Capture</a></h1>
+            <h1>
+                <Link to="/" id="logo">Capture</Link></h1>
             <ul>
                 <li>
                     <Link to="/">1. About Us</Link>
